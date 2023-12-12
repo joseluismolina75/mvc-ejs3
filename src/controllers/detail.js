8a860d9365f7c197f5063ae42eaf80efa287627c
joseluismolina75 <joseluismolina75@gmail.com>
2023-12-12 14:55:01 -0300

    modificaciones de detalle menú

--- src/controllers/detail.js
+++ src/controllers/detail.js
@@ -1,4 +1,4 @@
-const listaPlatos = require('../data/platos')
+const listaPlatos = require("../data/platos")
 
 const controller = {
     detail: (req, res) => {
@@ -6,7 +6,9 @@ const controller = {
         //const { id } = req.params;  las 2 formas son equivalentes, en la segunda uso destructuring y solo me devuelve el id
         const getProduct = listaPlatos.find(product => product.id == id); //tengo que saber que producto corresponde a dicho id
         if (getProduct) {
-            return res.render('detalleMenu', {getProduct});
+            console.log(getProduct);
+            console.log(id);
+            return res.render('detalleMenu', {listaPlatos});
         }
         res.send('El producto no existe');
     }
